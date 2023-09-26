c3ff9b86951ea86a5f7c0b2b9e202bea07e1caf5
Unreal <unrealhoang@gmail.com>
2023-09-26 12:22:08 +0900

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -150,6 +150,7 @@
   programs.seahorse.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  services.ratbagd.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
